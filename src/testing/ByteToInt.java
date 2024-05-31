a12c55db07498c2d9258fd7e6fbb9a6f8ac35bc0
yaswanthsaiG <ysgarigipati.mca@gmail.com>
2024-05-31 12:14:54 +0530

    first commit

---
+++ src/testing/ByteToInt.java
@@ -0,0 +1,10 @@
+package testing;
+
+public class ByteToInt {
+        public static void main(String[] args){
+            Byte b=12;        //here directly we are converting
+            int i=b;
+            System.out.println(i);
+    }
+
+}
---
+++ src/testing/IntToBite.java
@@ -0,0 +1,11 @@
+package testing;
+
+public class IntToBite {
+
+        public static void main(String[]args){
+            int a = -129;
+            byte b =( byte) a;
+        System.out.println(b);
+    }
+
+}
